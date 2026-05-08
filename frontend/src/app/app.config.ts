7276fc672b75a1f978f6dafedc3bfb3445150725
Alvaro Quispe UAI <alvaro.quispepena@autonomadeica.edu.pe>
2026-05-08 18:04:11 -0500

    fix

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,6 +3,7 @@ import {
   APP_INITIALIZER,
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { AuthService } from './core/auth.service';
@@ -11,6 +12,7 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection({ eventCoalescing: true, runCoalescing: true }),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
